2387237bd82e888645cec2094c89c55ccc166415
Kendra Ryan <yesthisiskendra@gmail.com>
2021-08-11 17:03:46 -0700

    Add button from home screen to details screen

--- App.js
+++ App.js
@@ -1,15 +1,27 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen!!!</Text>
+      <Text>Home Screen Party!!</Text>
+      <Button
+        title='Go to Details'
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen!!!</Text>
+//     </View>
+//   );
+// }
 
 function DetailsScreen() {
   return (
