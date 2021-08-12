8bd77a2dd0affe293f1803f30e036f85e6aa3250
Kendra Ryan <yesthisiskendra@gmail.com>
2021-08-11 17:27:18 -0700

    Add additional buttons to detail screen

--- App.js
+++ App.js
@@ -15,6 +15,24 @@ function HomeScreen({ navigation }) {
   );
 }
 
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title='Go to Details... again'
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title='Go to Home' onPress={() => navigation.navigate('Home')} />
+      <Button title='Go back' onPress={() => navigation.goBack()} />
+      <Button
+        title='Go back to first screen in stack'
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
 // function HomeScreen() {
 //   return (
 //     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -23,13 +41,13 @@ function HomeScreen({ navigation }) {
 //   );
 // }
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+// function DetailsScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Details Screen</Text>
+//     </View>
+//   );
+// }
 
 const Stack = createNativeStackNavigator();
 
